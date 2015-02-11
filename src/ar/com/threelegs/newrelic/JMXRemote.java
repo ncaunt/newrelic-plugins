81773e8d1e95c21b1067e434d62ff9d8f4acc4e8
Neil Caunt <neil.caunt@laterooms.com>
2015-02-11 15:00:15 +0000

    Support derived values using EpochCounter

--- src/ar/com/threelegs/newrelic/JMXRemote.java
+++ src/ar/com/threelegs/newrelic/JMXRemote.java
@@ -2,6 +2,7 @@ package ar.com.threelegs.newrelic;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.HashMap;
 
 import javax.management.MBeanServerConnection;
 import javax.management.ObjectName;
@@ -11,6 +12,7 @@ import ar.com.threelegs.newrelic.jmx.JMXHelper;
 import ar.com.threelegs.newrelic.jmx.JMXTemplate;
 
 import com.newrelic.metrics.publish.Agent;
+import com.newrelic.metrics.publish.processors.EpochCounter;
 import com.newrelic.metrics.publish.util.Logger;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigException;
@@ -19,6 +21,7 @@ public class JMXRemote extends Agent {
 	private static final Logger LOGGER = Logger.getLogger(JMXRemote.class);
 	private String name, host, port, metricPrefix;
 	private List<? extends Config> JMXList;
+	private HashMap<String, EpochCounter> derivatives;
 	
 	public JMXRemote(Config config) {
 		this(config, Defaults.JMXREMOTE_PLUGIN_NAME, Defaults.VERSION);
@@ -31,6 +34,24 @@ public class JMXRemote extends Agent {
 		this.port = config.getString("port");
 		this.JMXList = config.getConfigList("metrics");
 		this.metricPrefix = "JMX/hosts/" + host +":" + port;
+		this.derivatives = new HashMap<String, EpochCounter>();
+	}
+
+	private String createMetricName(String prefix, String name, String valueType) {
+		return metricPrefix + "/" + name.replaceAll("\"", "").replaceAll(":", "/").replaceAll(",", "/") + "/" + valueType;
+	}
+
+	private Number calculateValue(String derivativeKey, Number rawVal, Boolean isDerivative) {
+		if (isDerivative) {
+			if (!derivatives.containsKey(derivativeKey)) {
+				LOGGER.debug("creating new EpochCounter for '" + derivativeKey + "'");
+				derivatives.put(derivativeKey, new EpochCounter());
+			}
+
+			return derivatives.get(derivativeKey).process(rawVal);
+		}
+
+		return rawVal;
 	}
 
 	@Override
@@ -53,14 +74,23 @@ public class JMXRemote extends Agent {
 							for (Config thisMetric : myList) {
 								ObjectName thisObjectName = ObjectName.getInstance(thisMetric.getString("objectname"));
 								String thisMetricType;
+								Boolean isDerivative = false;
 								try { thisMetricType = thisMetric.getString("type"); } catch (ConfigException e) { thisMetricType = "value"; }
+								try {
+									isDerivative = thisMetric.getBoolean("derive");
+								} catch (ConfigException e) {}
+
 								try { 
 									// Using <Metric> with different assignments for the 2 strings in it.
 									List<Metric> resultValues = JMXHelper.queryAndGetAttributes(connection, thisObjectName, thisMetric.getStringList("attributes"));
 									for (Metric thisValue : resultValues) {
-										String metricName = metricPrefix + "/" + thisValue.name.replaceAll("\"", "").replaceAll(":", "/").replaceAll(",", "/") + "/" + thisValue.valueType;
+										String metricName = createMetricName(metricPrefix, thisValue.name, thisValue.valueType);
 										// Adding actual metric in proper form to metric list.
-										metrics.add(new Metric(metricName, thisMetricType, thisValue.value));
+
+										String derivativeKey = metricName;
+										Number value = calculateValue(metricName, thisValue.value, isDerivative);
+
+										metrics.add(new Metric(metricName, thisMetricType, value));
 									}
 								} catch (Exception e) {
 									LOGGER.error(e, "failed object: " + thisMetric.getString("objectname"));
